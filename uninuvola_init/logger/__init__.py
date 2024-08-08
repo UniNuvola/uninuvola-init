c2b6511b7914b804f83682ff9ffdbbeacf378707
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-08-08 17:51:39 +0200

    📝 (logger/__init__.py): add a TODO
    Added comment about saving logs to a file and consider using structlog library for better logging structure.

--- uninuvola_init/logger/__init__.py
+++ uninuvola_init/logger/__init__.py
@@ -3,6 +3,7 @@ from uninuvola_init.args import args
 
 # Logging settings
 ## TODO: salvare logs su file ?
+## TODO: consider using structlog
 logging.basicConfig(
     level=args.verbose.upper(),
     format='%(asctime)s - %(levelname)s: %(message)s',
