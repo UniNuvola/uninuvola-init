9c58a2e921785552a2ba985fee7fbc6157a454fd
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-08-08 17:50:00 +0200

    ✨ (uninuvola_init): add package for parse yaml files

--- uninuvola_init/cli.py
+++ uninuvola_init/cli.py
@@ -2,7 +2,9 @@ from uninuvola_init import vault
 from uninuvola_init import openLDAP
 from uninuvola_init.logger import logger
 from uninuvola_init.args import args
+from uninuvola_init.config_parser import config_parser
 
 def main():
     logger.info("hello world !")
     logger.debug("Debug info")
+    configs = config_parser(args.config)
---
+++ uninuvola_init/config_parser/__init__.py
@@ -0,0 +1,22 @@
+import sys
+from yaml import safe_load, YAMLError
+from uninuvola_init.logger import logger
+
+
+def config_parser(path) -> dict:
+    try:
+        with open(path, 'r') as stream:
+            try:
+                content = safe_load(stream)
+
+                logger.debug("Config file '%s': %s", path, content)
+
+                return content
+
+            except YAMLError as e:
+                logger.error("A problem occur with filei '%s':\n%s", path, e)
+                sys.exit(1)
+
+    except FileNotFoundError as e:
+        logger.error("%s: %s", e, path)
+        sys.exit(1)
