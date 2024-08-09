3dfff553d45aa0beba52dc0eeb4648da23533896
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-08-09 10:31:38 +0200

    ✨ (uninuvola_init/vault): add vault apis
    Added vault module to control and initialize the Vault docker container
    instance.
    Updated config.yaml accordingly

--- uninuvola_init/cli.py
+++ uninuvola_init/cli.py
@@ -2,9 +2,7 @@ from uninuvola_init import vault
 from uninuvola_init import openLDAP
 from uninuvola_init.logger import logger
 from uninuvola_init.args import args
-from uninuvola_init.config_parser import config_parser
+from uninuvola_init.configs import configs
 
 def main():
-    logger.info("hello world !")
-    logger.debug("Debug info")
-    configs = config_parser(args.config)
+    vault.deploy()
--- uninuvola_init/vault/__init__.py
+++ uninuvola_init/vault/__init__.py
@@ -1,1 +1,27 @@
-print("vault")
+from uninuvola_init.configs import configs
+from . import api
+
+
+def deploy():
+    """Deploy procedure.
+
+    Runs every CLI commands needed for auto-deploy a new Vault container.
+    """
+
+    api.init()
+    api.unseal()
+    # api.enable_userpass()
+    # api.create_entity()
+    # api.set_users()
+    api.create_group()
+
+    # creating oidc application and retriving configs
+    for _, oidc_conf in configs['vault']['oidcs'].items():
+        api.oidc(
+            appname=oidc_conf['appname'],
+            scopename=oidc_conf['scopename'],
+            providername=oidc_conf['providername'],
+            redirect_uris=oidc_conf['redirect_uris'],
+        )
+
+        api.get_config(appname='web', filename=oidc_conf['secretfile'])
