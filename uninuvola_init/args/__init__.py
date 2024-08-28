0785a7778e8f6e95cfb80f1fbaf638daf65d9bdf
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-08-28 15:19:00 +0200

    📝 (args/__init__.py): add new argument "services"
    Added new cli argument to allow specifying which service to configure when running the script.

--- uninuvola_init/args/__init__.py
+++ uninuvola_init/args/__init__.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 _parser = argparse.ArgumentParser()
 _parser.add_argument(
     "-v", "--verbose",
@@ -13,6 +14,17 @@ _parser.add_argument(
     type=str,
     # required=True,
     help="Config file in yaml format"
+    )
+_parser.add_argument(
+    "--services", 
+    type=str,
+    nargs='+',
+    # default=[]
+    help="""Specify which service should be configured.
+    Available:
+        - 'ldap'
+        - 'vault'
+    """
     )
 
 
