e8f728ec9574a9f2d6b55d6e63c8984e2e088afd
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-10-16 14:04:12 +0000

    ♻️ (uninuvola_init/openLDAP): update users/groups creation (#10)
    Updated openLDAP users/groups creation workflow: no more users are created and groups names are taken from `openldap.groups` yaml config.
    
    ## Commits
    
    * 🔧 (uninuvola_init/openLDAP): update users/groups creation
    
    Updated openLDAP users/groups creation workflow: no more users are
    created and groups names are taken from `openldap.groups` yaml config.
    
    * ♻️ (uninuvola_init/openLDAP): change group order
    
    Users group must have 500 group id

--- uninuvola_init/openLDAP/__init__.py
+++ uninuvola_init/openLDAP/__init__.py
@@ -1,4 +1,5 @@
 from uninuvola_init.logger import logger 
+from uninuvola_init.configs import configs
 from .api import LdapManager
 
 
@@ -6,5 +7,5 @@ def deploy():
     logger.info("Deploying OPENLDAP")
 
     manager = LdapManager()
-    manager.add_groups()
-    manager.add_users()
+    manager.add_groups(groups=configs['openldap']['groups'])
+    # manager.add_users()
